6e3da1f966a75af071d2f3ed8480bb8f3171bfe6
khiyam <khiyamiftikhar@gmail.com>
2025-10-19 22:21:13 +0500

    Sub branching approach to be used now onwards

--- components/http-server/http_server.c
+++ components/http-server/http_server.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 #include "freertos/queue.h"
 #include <string.h>
 #include <esp_log.h>
@@ -7,6 +8,7 @@
 #include "cJSON.h"
 #include "esp_random.h"     // esp_random()
 #include "http_server.h"
+#include "event_system_adapter.h"
 
 
 #define         MAX_URIS                    5
@@ -42,19 +44,21 @@ typedef struct {
     bool active;
     char username[32];
     char token[64];
-    time_t last_activity;
+    uint32_t last_activity;
 } session_t;
 
 static session_t current_sessions[MAX_USERS] = {0};
 
 
-
-#define UPLOAD_CHUNK_SIZE 1024
+//Multiple buffers/buckets in the pool
+//Sent to ota, and ota returns after usage
+//Meanwhile the file upload uses  other from the pool if available
+#define TOTAL_CHUNK_BUFFERS      3      
+#define UPLOAD_CHUNK_SIZE       1024    
 
 static struct{
     httpd_handle_t server_handle;
-    char recv_buf1[UPLOAD_CHUNK_SIZE];   
-    char recv_buf2[UPLOAD_CHUNK_SIZE];   
+    char recv_buf[TOTAL_CHUNK_BUFFERS][UPLOAD_CHUNK_SIZE];   
     QueueHandle_t buffer_queue;
    // bool logged_in;
     
@@ -81,9 +85,33 @@ static const char *TAG = "HTTP Server";
 
 
 
+DEFINE_EVENT_ADAPTER(HTTP_SERVER);
 
 
+/// @brief Get a buffer from pool
+/// @param wait_time in ms
+/// @return 
+static char* get_chunk_buffer_from_pool(uint32_t wait_time){
 
+    char* recv_buf;
+    //Specifying the address of pointer, because an address is to be copied
+    if(xQueueReceive(http_server.buffer_queue,&recv_buf,pdMS_TO_TICKS(wait_time))!=pdTRUE)
+        return NULL;
+
+    ESP_LOGI(TAG,"buff address %p",recv_buf);
+    return recv_buf;
+}
+
+esp_err_t http_server_return_chunk_buffer(char* buffer){
+    
+    
+    //Supplies address  of pointer to get data, so copies address of mem it points
+    ESP_LOGI(TAG,"ret address %p",buffer);
+    if(xQueueSend(http_server.buffer_queue,&buffer,portMAX_DELAY)!=pdTRUE)
+        return ESP_FAIL;
+
+    return ESP_OK;
+}
 
 static bool is_user_logged_in(void) {
     for (int i = 0; i < MAX_USERS; ++i) {
@@ -115,14 +143,14 @@ static bool validate_token(const char *token)
         if (current_sessions[i].active &&
             strcmp(current_sessions[i].token, token) == 0) {
 
-            // Optional timeout check: 30 min session
-            //if (difftime(now, current_sessions[i].last_activity) > 1800) {
-              //  current_sessions[i].active = false;
-                //return false;
-            //}
+            uint32_t current_ticks=xTaskGetTickCount()*portTICK_PERIOD_MS;
+            if ((current_ticks - current_sessions[i].last_activity) > 1800) {
+                current_sessions[i].active = false;
+                return false;
+            }
 
             // update last activity
-            //current_sessions[i].last_activity = now;
+            current_sessions[i].last_activity = current_ticks;
             ESP_LOGI(TAG,"match found");
             return true;
         }
@@ -197,12 +225,13 @@ static esp_err_t auth_handler(httpd_req_t *req)
         generate_token(token, sizeof(token));
 
     for (int i = 0; i < MAX_USERS; ++i) {
+
         if (!current_sessions[i].active) {
             ESP_LOGI(TAG,"saving token");
             current_sessions[i].active = true;
             //strncpy(current_sessions[i].username, username, sizeof(current_sessions[i].username));
             strncpy(current_sessions[i].token, token, sizeof(current_sessions[i].token));
-            //current_sessions[i].last_activity = time(NULL);
+            current_sessions[i].last_activity=xTaskGetTickCount()*portTICK_PERIOD_MS; //Time in milliseconds
             break;
         }
     }
@@ -317,43 +346,57 @@ static esp_err_t check_auth_handler(httpd_req_t *req)
 static esp_err_t file_upload_handler(httpd_req_t *req)
 {
         
-    esp_err_t ret=logged_in_checked(req);
-
-    //ESP_LOGI(TAG,"logged status chhecked");
-    if(ret!=ESP_OK)
-        return ret;
+    
     
     //ESP_LOGI(TAG,"logged user");
     int received;
 
     char* recv_buf;
     
-    //Get the buffer from pool
-    if(xQueueReceive(http_server.buffer_queue,&recv_buf,pdMS_TO_TICKS(10))!=pdTRUE)
-        return ESP_FAIL;
-
+    
+    
+    //Post that file transfer is started, so that ota perfomrs thee inits required
+    //It is written outside assuming the first buffer  will always be retereived so no risk
+    //Because otherwise this signal must be sent if buffer is  acquired
+    HTTP_SERVER_post_event(HTTP_SERVER_EVENT_FILE_TRANSFER_STARTED,NULL,0);         
+    
     ESP_LOGI(TAG, "File upload started, content length: %d", req->content_len);
-
     size_t total_received = 0;
+    while(1){
 
-    while ((received = httpd_req_recv(req, recv_buf, sizeof(recv_buf))) > 0) {
+        char* recv_buf=NULL;
+        recv_buf=get_chunk_buffer_from_pool(10);
+
+        if(recv_buf==NULL){
+            ESP_LOGE(TAG, "Mem not available");
+            httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Mem not available");
+            return ESP_FAIL;        
+        }
+        
+        received = httpd_req_recv(req, recv_buf, UPLOAD_CHUNK_SIZE);
+
+        if (received < 0) {
+        ESP_LOGE(TAG, "File receive error: %d", received);
+        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Receive error");
+        HTTP_SERVER_post_event(HTTP_SERVER_EVENT_FILE_TRASFER_FAILED,NULL,0);         
+        return ESP_FAIL;
+        }
+
+        if (received == 0) {
+            ESP_LOGE(TAG, "File receive complete");
+            //httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Receive error");
+            HTTP_SERVER_post_event(HTTP_SERVER_EVENT_FILE_TRANSFER_COMPLETE,NULL,0);         
+            break;
+         }
         total_received += received;
 
-        // === You will later post this buffer to your OTA/event handler ===
-        // Example:
-        // ota_chunk_t chunk;
-        // memcpy(chunk.data, recv_buf, received);
-        // chunk.len = received;
-        // esp_event_post_to(ota_event_loop, OTA_EVENT_DATA, &chunk, sizeof(chunk), portMAX_DELAY);
+        HTTP_SERVER_post_event(HTTP_SERVER_EVENT_FILE_CHUNK_ARRIVED,(const void*)recv_buf,received);
+            
 
         ESP_LOGD(TAG, "Received %d bytes (total %u)", received, total_received);
     }
 
-    if (received < 0) {
-        ESP_LOGE(TAG, "File receive error: %d", received);
-        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Receive error");
-        return ESP_FAIL;
-    }
+    
 
     ESP_LOGI(TAG, "File upload completed, total received: %u bytes", total_received);
 
@@ -363,7 +406,7 @@ static esp_err_t file_upload_handler(httpd_req_t *req)
     httpd_resp_sendstr(req, "Upload successful");
 
     //Just for testing . this mustb be called by ota
-    xQueueSend(http_server.buffer_queue,&recv_buf,portMAX_DELAY);
+    
     return ESP_OK;
 }
 
@@ -570,23 +613,21 @@ esp_err_t http_server_init(http_server_config_t* config){
     http_config.server_port = config->port;
     http_config.max_uri_handlers=13;
     ESP_LOGI(TAG, "Entered Server Init");
-    http_server.buffer_queue=xQueueCreate(TOTAL_BUFFERS,sizeof(char*));
-
+    http_server.buffer_queue=xQueueCreate(TOTAL_CHUNK_BUFFERS,sizeof(char*));
 
     if(http_server.buffer_queue==NULL)
         return ESP_FAIL;
 
-    char* recv1=http_server.recv_buf1;
-    char* recv2=http_server.recv_buf2;
-
-    //Filling the pool
-    xQueueSend(http_server.buffer_queue,&recv1,portMAX_DELAY);
-    xQueueSend(http_server.buffer_queue,&recv2,portMAX_DELAY);
+    for(uint8_t i=0;i<TOTAL_CHUNK_BUFFERS;i++){
+        
+        char* p=http_server.recv_buf[i];
+        ESP_LOGI(TAG,"buffer addresses %p",p);
+        xQueueSend(http_server.buffer_queue,&p,portMAX_DELAY);
+    }
+    
 
     ESP_LOGI(TAG, "Starting HTTP Server");
     if( httpd_start(&http_server.server_handle, &http_config) != ESP_OK){
-
-    
         ESP_LOGI(TAG,"Server Init Failed");
         return ESP_FAIL;
     }
@@ -702,11 +743,18 @@ esp_err_t http_server_init(http_server_config_t* config){
     httpd_register_uri_handler(http_server.server_handle, &checkauth_uri);
 
 
-    
 
-
+    //Tell the ota that file chunk will be transferred next
+    HTTP_SERVER_register_event(HTTP_SERVER_EVENT_FILE_TRANSFER_STARTED,NULL);
+    //When a file chunk arrives
+    HTTP_SERVER_register_event(HTTP_SERVER_EVENT_FILE_CHUNK_ARRIVED,NULL);
+    //When file recive fails
+    HTTP_SERVER_register_event(HTTP_SERVER_EVENT_FILE_TRASFER_FAILED,NULL);
+    //On successfull EOF (0 data recevied in last chunk)
+    HTTP_SERVER_register_event(HTTP_SERVER_EVENT_FILE_TRANSFER_COMPLETE,NULL);
     return ESP_OK;
 
+    
 }
 
 
--- components/http-server/http_server.h
+++ components/http-server/http_server.h
@@ -5,7 +5,20 @@
 
 
     
+//The below is  equivalent 
+//ESP_EVENT_DECLARE_BASE(MY_MODULE_NAME_ROUTINE_EVENT_BASE);
+//but this way it does not require to include the esp_event header
+extern const char * const HTTP_SERVER_ROUTINE_EVENT_BASE;
 
+//The below is  equivalent 
+//ESP_EVENT_DECLARE_BASE(MY_MODULE_NAME_EXCEPTION_EVENT_BASE);
+extern const char * const HTTP_SERVER_EXCEPTION_EVENT_BASE;
+
+
+#define   HTTP_SERVER_EVENT_FILE_TRANSFER_STARTED       1
+#define   HTTP_SERVER_EVENT_FILE_CHUNK_ARRIVED          2
+#define   HTTP_SERVER_EVENT_FILE_TRASFER_FAILED         3
+#define   HTTP_SERVER_EVENT_FILE_TRANSFER_COMPLETE      4
 
 typedef enum {
     PROTOCOL_HTTP,          // Regular HTTP
@@ -26,5 +39,6 @@ typedef struct {
 
 
 esp_err_t http_server_init(http_server_config_t* config);
+esp_err_t http_server_return_chunk_buffer(char* buffer);
 
 #endif
