4cbb7542e714811e4a08cfa6e5a35b3a2f45096b
khiyam <khiyamiftikhar@gmail.com>
2025-12-19 12:51:53 +0500

    new methods added to set individual feilds

--- components/gui-component/priv_include/ui_boot.h
+++ components/gui-component/priv_include/ui_boot.h
@@ -10,6 +10,10 @@ extern "C" {
 void ui_boot_init(void);
 void ui_boot_load_screen();
 
+void ui_boot_set_main_label(char* string);
+void ui_boot_set_channel_info_label(char* string);
+void ui_boot_set_discovery_info_label(char* string);
+
 #ifdef __cplusplus
 }
 #endif
