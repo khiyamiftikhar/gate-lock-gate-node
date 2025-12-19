a300655b7df6243d2afbbece9394e7a484eae069
khiyam <khiyamiftikhar@gmail.com>
2025-12-19 12:54:03 +0500

    new methods added to set individual feilds

--- components/gui-component/priv_src/boot_screen.c
+++ components/gui-component/priv_src/boot_screen.c
@@ -71,10 +71,14 @@ void ui_boot_load_screen(){
 
 
 
-static void ui_boot_set_main_label_cb(void* args){
-    printf("\nboot screen...");
-    lv_scr_load(boot_screen.lv_screen);
-    printf("\nloaded...\n");
+static void ui_boot_set_label_cb(void* args){
+    
+    uint8_t child_index=(int32_t) args;
+    ui_child_t *c = &boot_screen.children[child_index];
+
+    char* text = c->text;
+    
+    lv_label_set_text(c->lv_obj, text);  // update LVGL object
 
      //for(int i = 0; i < boot_screen.child_count; i++){
        // ui_child_t *c = &boot_screen.children[i];
@@ -83,8 +87,26 @@ static void ui_boot_set_main_label_cb(void* args){
 
 void ui_boot_set_main_label(char* string){
 
+    int n = snprintf(boot_screen.children[0].text, sizeof(boot_screen.children[0].text), "%s", string);
+    //Send the id of the child label
+    ui_worker_process_job(ui_boot_set_label_cb, (void*)0);
+
+}
+
+
+void ui_boot_set_channel_info_label(char* string){
+
     int n = snprintf(boot_screen.children[1].text, sizeof(boot_screen.children[1].text), "%s", string);
-    ui_worker_process_job(ui_boot_set_main_label_cb, (void*)boot_screen.children[1].text);
+    //Send the id of the child label
+    ui_worker_process_job(ui_boot_set_label_cb, (void*)1);
+
+}
+
+void ui_boot_set_discovery_info_label(char* string){
+
+    int n = snprintf(boot_screen.children[2].text, sizeof(boot_screen.children[2].text), "%s", string);
+    //Send the id of the child label
+    ui_worker_process_job(ui_boot_set_label_cb, (void*)2);
 
 }
 
@@ -105,15 +127,27 @@ void ui_boot_init(void)
             case UI_CHILD_ICON:
                 c->lv_obj = lv_img_create(boot_screen.lv_screen);
                 lv_obj_set_pos(c->lv_obj, c->x, c->y);
-                lv_image_set_inner_align(c->lv_obj, LV_IMAGE_ALIGN_TOP_MID);
+
+                /* enforce icon boundaries */
+                lv_obj_set_size(c->lv_obj, c->w, c->h);
+                lv_obj_set_style_clip_corner(
+                    c->lv_obj,
+                    true,
+                    LV_PART_MAIN | LV_STATE_DEFAULT
+                );
+
+                lv_image_set_inner_align(c->lv_obj, LV_IMAGE_ALIGN_CENTER);
+
                 // don't set src here; do it in separate loader
                 break;
+
 
             case UI_CHILD_LABEL:
                 c->lv_obj = lv_label_create(boot_screen.lv_screen);
                 lv_obj_set_pos(c->lv_obj, c->x, c->y);
                 lv_obj_set_style_pad_all(c->lv_obj, 0, 0);
-                lv_obj_set_size(c->lv_obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+                lv_obj_set_width(c->lv_obj, c->w);
+                lv_label_set_long_mode(c->lv_obj, LV_LABEL_LONG_CLIP);
                 // text will be set separately at runtime
                 break;
 
